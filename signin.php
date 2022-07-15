4f791f7400bbfa107b4f4d0967bb4f485df54741
ksh30918 <ksh30918@kakao.com>
2022-07-15 22:54:45 +0900

    edit

---
+++ signin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>로그인</title>
+</head>
+
+<body>
+	
+</body>
+	<?php
+		if($_SERVER['REQUEST_METHOD'] === 'POST')
+		{
+			$id = $_POST['txtId'];
+			$password = $_POST['txtPassowrd'];
+
+			if($id == $password)
+			echo '<h1>로그인 성공</h1>';
+			else
+			echo '<h1>로그인 실패</h1>';
+		}
+	?>
+</html>
