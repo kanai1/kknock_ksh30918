cf7b11327d3765999ba6533efc8c9bacf467ceb4
ksh30918 <ksh30918@kakao.com>
2022-07-16 11:19:40 +0900

    edit

--- signin.php
+++ signin.php
@@ -24,17 +24,5 @@
 
 			echo "ID: {$id}<br>Password: {$password}";
 		}
-		else
-		{
-			$id = $_GET['txtId'];
-			$password = $_GET['txtPassword'];
-
-			if($id == $password)
-				echo '<h1>로그인 성공</h1>';
-			else
-				echo '<h1>로그인 실패</h1>';
-
-			echo "ID: {$id}<br>Password: {$password}";
-		}
 	?>
 </html>
