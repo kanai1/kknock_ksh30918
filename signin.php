e68a92dd5cde0967e018485f6c16fb2bda29f304
ksh30918 <ksh30918@kakao.com>
2022-07-20 10:20:44 +0900

    add mysql in signin.php

--- signin.php
+++ signin.php
@@ -13,12 +13,30 @@
 			$id = $_POST['txtId'];
 			$password = $_POST['txtPassword'];
 
-			if($id == $password)
-				echo '<h1>로그인 성공</h1>';
-			else
-				echo '<h1>로그인 실패</h1>';
+			$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
 
-			echo "ID: {$id}<br>Password: {$password}";
+			$sql = "SELECT * FROM user_login where login_id='$id' && login_pw='$password'";
+
+			if($result = mysqli_fetch_array(mysqli_query($conn, $sql)))
+			{
+				$heredoc = <<< HERE
+				<h2>로그인 성공</h2>
+				<br>사용자 아이디: {$result['login_id']}
+				<br>아이디 생성시간: {$result['created']}
+				<br><button onclick="history.back()">돌아가기</button>
+				HERE;
+
+				echo $heredoc;
+			}
+			else
+			{
+				$heredoc = <<< HERE
+				<h2>로그인 실패</h2>
+				<br><button onclick="history.back()">돌아가기</button>
+				HERE;
+
+				echo $heredoc;
+			}
 		}
 	?>
 </body>
