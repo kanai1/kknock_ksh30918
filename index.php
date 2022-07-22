178183f6068347ade29234f233be8ab729818936
ksh30918 <ksh30918@kakao.com>
2022-07-22 20:24:55 +0900

    edit

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 				{
 					$heredoc = <<< HERE
 					{$_SESSION['user_name']}님 어서오세요
-					<button onclick="location.href='index.php'">로그아웃</button>
+					<button onclick="location.href='logout.php'">로그아웃</button>
 					HERE;
 
 					echo $heredoc;
