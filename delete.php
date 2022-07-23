3a85584fd7bb5276f84947fe63fc8d4e20a3758b
ksh30918 <ksh30918@kakao.com>
2022-07-23 12:51:22 +0900

    add delete.php

---
+++ delete.php
@@ -0,0 +1,41 @@
+<?php
+	$wrong_connection = <<< HERE
+	<script>
+	alart('잘못된 접근입니다.');
+	location.replace('/');
+	<script>
+	HERE;
+
+	if(!isset($_SESSION['user_id']))
+	{
+		echo $wrong_connection;
+	}
+	if($_SERVER['REQUEST_METHOD'] == 'GET')
+	{
+		$post_num = $_GET['number'];
+
+		$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
+		$sql_find = "SELECT * FROM board WHERE post_num=$post_num";
+
+		if(mysqli_query($conn, $sql_find))
+		{
+			$sql_delete = "DELETE FROM board WHERE post_num = $post_num";
+			if(mysqli_query($conn, $sql_delete))
+			{
+				$heredoc = <<< HERE
+				<script>
+				alert('게시글을 삭제했습니다.')
+				location.replace('/');
+				</script>
+				HERE;
+
+				echo $heredoc;
+			}
+		}
+		else
+		{
+			echo $wrong_connection;
+		}
+
+	}
+?>
