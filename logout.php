5ee74c07a70693a7c8b15ca6b9215eda561ed91d
ksh30918 <ksh30918@kakao.com>
2022-07-23 10:26:18 +0900

    edit session

--- logout.php
+++ logout.php
@@ -1,17 +1,6 @@
 <?php
 	session_start();
-
 	session_unset();
-	
-	if (ini_get("session.use_cookies")) {
-		$params = session_get_cookie_params();
-		setcookie(
-		  session_name(), '', time() - 42000,
-		  $params["path"], $params["domain"],
-		  $params["secure"], $params["httponly"]
-		);
-	  }
-
 	session_destroy();
-	echo "<script>location.href='index.php'</script>";
+	echo "<script>location.href='/'</script>";
 ?>
