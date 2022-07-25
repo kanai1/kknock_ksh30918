1a3d45267db87a2783c3486c43c0b95b9e4ebe84
ksh30918 <ksh30918@kakao.com>
2022-07-25 14:26:57 +0900

    edit

--- withdrawal_ok.php
+++ withdrawal_ok.php
@@ -10,47 +10,46 @@
 	{
 		echo $wrong_connection;
 	}
-	if(strcmp($_SERVER['REQUEST_METHOD'], "POST"))
+	if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
-		echo $wrong_connection;
-	}
-
-	$password = $_POST['userPassword'];
-	$passwordAgain = $_POST['userPasswordAgain'];
-
-	if(strcmp($password, $passwordAgain))
-	{
-		$heredoc = <<< HERE
-		<script>
-		alert('비밀번호가 다릅니다.');
-		history.back();
-		</script>
-		HERE;
-	}
-
-	$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
-	$sql_find = "SELECT * FROM user_login WHERE login_id = '{$_SESSION['user_id']}' && user_pw = '{$password}'";
-
-	if(mysqli_fetch_array(mysqli_query($conn, $sql_find)) == "")
-	{
-		echo $wrong_connection;
-	}
-
-	$sql_delete = "DELETE FROM user_login WHERE login_id = '{$_SESSION['user_id']}'";
+		$password = $_POST['userPassword'];
+		$passwordAgain = $_POST['userPasswordAgain'];
 	
-	if(mysqli_query($conn, $sql_delete))
-	{
-		session_start();
-		session_unset();
-		session_destroy();
-
-		$heredoc = <<< HERE
-		<script>
-		alert('정상적으로 회원탈퇴되었습니다.');
-		location.replace('/');
-		</script>
-		HERE;
-
-		echo $heredoc;
-	}	
+		if(strcmp($password, $passwordAgain))
+		{
+			$heredoc = <<< HERE
+			<script>
+			alert('비밀번호가 다릅니다.');
+			history.back();
+			</script>
+			HERE;
+		}
+	
+		$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
+		$sql_find = "SELECT * FROM user_login WHERE login_id = '{$_SESSION['user_id']}' && user_pw = '{$password}'";
+	
+		if(mysqli_fetch_array(mysqli_query($conn, $sql_find)) == "")
+		{
+			echo $wrong_connection;
+		}
+	
+		$sql_delete = "DELETE FROM user_login WHERE login_id = '{$_SESSION['user_id']}'";
+		
+		if(mysqli_query($conn, $sql_delete))
+		{
+			session_start();
+			session_unset();
+			session_destroy();
+	
+			$heredoc = <<< HERE
+			<script>
+			alert('정상적으로 회원탈퇴되었습니다.');
+			location.replace('/');
+			</script>
+			HERE;
+	
+			echo $heredoc;
+		}
+	}
+	echo $wrong_connection;
 ?>
