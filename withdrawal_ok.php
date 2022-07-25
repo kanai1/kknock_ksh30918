c69096dbcfda07c61089fd5df15029a50a7113d7
ksh30918 <ksh30918@kakao.com>
2022-07-25 14:31:35 +0900

    edit mysql query

--- withdrawal_ok.php
+++ withdrawal_ok.php
@@ -26,7 +26,7 @@
 		}
 	
 		$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
-		$sql_find = "SELECT * FROM user_login WHERE login_id = '{$_SESSION['user_id']}' && user_pw = '{$password}'";
+		$sql_find = "SELECT * FROM user_login WHERE login_id = '{$_SESSION['user_id']}' && login_pw = '{$password}'";
 	
 		if(mysqli_fetch_array(mysqli_query($conn, $sql_find)) == "")
 		{
