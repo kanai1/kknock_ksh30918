d97d6037be6313c931d8c783a011c080aeeae74f
ksh30918 <ksh30918@kakao.com>
2022-07-28 11:15:50 +0900

    edit js and form

--- edit_comment.js
+++ edit_comment.js
@@ -1,8 +1,9 @@
 function comment_edit(button_id)
 {
+	var button = document.getElementById(button_id);
 	var comment_num = button_id.split('#')[1];
 	var comment_text = document.getElementById('text#'+comment_num);
 
 	comment_text.removeAttribute('readonly');
-	this.type = "summit";
+	button.type = "summit";
 }
