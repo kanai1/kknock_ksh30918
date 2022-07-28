05aeffa1e38b0a852e0b826572f2bd7a6ca8de0f
ksh30918 <ksh30918@kakao.com>
2022-07-28 11:07:42 +0900

    edit js

--- edit_comment.js
+++ edit_comment.js
@@ -1,6 +1,6 @@
 function comment_edit(button_id)
 {
-	var comment_num = button_id.split('#');
+	var comment_num = button_id.split('#')[1];
 	var comment_text = document.getElementById('text#'+comment_num);
 
 	comment_text.removeAttribute('readonly');
