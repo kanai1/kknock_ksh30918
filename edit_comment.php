42ec3f18558bef3f6003436a2b6122b610b892f6
ksh30918 <ksh30918@kakao.com>
2022-07-28 11:57:31 +0900

    add edit_comment.php

--- edit_comment.php
+++ edit_comment.php
@@ -10,12 +10,28 @@
 	{
 		echo $wrong_connection;
 	}
-	else// if($_SERVER['REQUEST_METHOD'] == "POST")
+	else if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
 		$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
-		$sql = "";
+		
+		$comment_num = $_GET['number'];
+		$comment = $_POST['comment'];
+		
+		$sql_find = "SELECT * FROM comment WHERE comment_num = $comment_num";
 
-		echo $_GET['number'];
-		echo $_POST['comment'];
+		$find_result = mysqli_fetch_array(mysqli_query($conn, $sql_find));
+
+		if($find_result == "" || strcmp($find_result['user_id'], $_SESSION['user_id']))
+		{
+			echo $wrong_connection;
+		}
+
+		$sql_update = "UPDATE comment SET comment='{$comment}' WHERE comment_num = $comment_num";
+
+		if(mysqli_query($conn, $sql_update))
+		{
+			echo "<script>history.back()</script>";
+		}
 	}
+	echo $wrong_connection;
 ?>
