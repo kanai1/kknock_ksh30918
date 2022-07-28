a7200d35e8f9e00437658058c6b849a29cb967fa
ksh30918 <ksh30918@kakao.com>
2022-07-28 11:18:02 +0900

    test

---
+++ edit_comment.php
@@ -0,0 +1,22 @@
+<?php
+	$wrong_connection = <<< HERE
+	<script>
+	alert('잘못된 접근입니다.');
+	location.replace('/');
+	</script>
+	HERE;
+
+	if(!isset($_SESSION['user_id']))
+	{
+		echo $wrong_connection;
+	}
+	else if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
+		$sql = "";
+
+		echo $_GET['number'];
+		echo $_POST['comment'];
+	}
+	echo $wrong_connection;
+?>
