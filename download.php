9a92191635d12ca5935033165473f22491be9d20
ksh30918 <ksh30918@kakao.com>
2022-08-08 10:42:20 +0900

    edit

--- download.php
+++ download.php
@@ -22,7 +22,7 @@
 		$filename = $name;
 
 		header("Content-Type: application/octet-stream");
-		header("Content-Disposition: attachment; filename='$filename'");
+		header("Content-Disposition: attachment; filename=$filename");
 		header("Content-Transfer-Encoding: binary");
 		header("Content-Length: $filesize");
 
