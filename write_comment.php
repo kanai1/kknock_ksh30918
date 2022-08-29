04275ed62562a595bbf0d2c86de43522e6d83ce8
ksh30918 <ksh30918@kakao.com>
2022-08-29 19:28:18 +0900

    comment stmt

--- write_comment.php
+++ write_comment.php
@@ -15,9 +15,12 @@
 		$comment = $_POST['comment'];
 
 		$conn = mysqli_connect('localhost', 'TeamA', 'TeamA1234567@', 'test');
-		$sql = "INSERT INTO comment(post_num, user_id, user_name, comment, commented) VALUE ({$_GET['number']}, '{$_SESSION['user_id']}', '{$_SESSION['user_name']}', '{$comment}', now())";
+		$sql = "INSERT INTO comment(post_num, user_id, user_name, comment, commented) VALUE (?, ?, ?, ?, now())";
+		$stmt = mysqli_stmt_init($conn);
+		mysqli_stmt_prepare($stmt, $sql);
+		mysqli_stmt_bind_param($stmt, '', $_GET['number'], $_SESSION['user_id'], $_SESSION['user_name'], $comment);
 
-		if(!mysqli_query($conn, $sql))
+		if(!mysqli_stmt_execute($stmt))
 		{
 			echo $wrong_connection;
 		}
