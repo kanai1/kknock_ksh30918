4f9350b9f1de5c41942e8c4ca130ee56f7864222
ksh30918 <ksh30918@kakao.com>
2022-08-29 18:28:49 +0900

    test

--- search.php
+++ search.php
@@ -39,10 +39,10 @@
 		$sql = mysqli_stmt_init($conn);
 		$sql_index = mysqli_stmt_init($conn);
 		mysqli_stmt_prepare($sql, "SELECT * FROM board WHERE title LIKE '%?%' OR user_name LIKE '%?%' ORDER BY post_num ".$order);
-		mysqli_stmt_prepare($sql_index, "SELECT * FROM board WHERE title LIKE '%?%' OR user_name LIKE '%?%' ORDER BY post_num ? LIMIT ?, 10");
+		mysqli_stmt_prepare($sql_index, "SELECT * FROM board WHERE title LIKE '%?%' OR user_name LIKE '%?%' ORDER BY post_num ".$order." LIMIT ?, 10");
 
 		mysqli_stmt_bind_param($sql, 'ss', $query, $query);
-		mysqli_stmt_bind_param($sql_index, 'sss', $query, $query, $order, $start_num);
+		mysqli_stmt_bind_param($sql_index, 'sss', $query, $query, $start_num);
 		mysqli_stmt_execute($sql);
 		mysqli_stmt_execute($sql_index);
 
