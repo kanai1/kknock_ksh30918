f8f4dac9e2753f4f19d2cea4c03fc87a9b4c6e4b
ksh30918 <ksh30918@kakao.com>
2022-08-29 20:17:52 +0900

    typo

--- edit_comment.php
+++ edit_comment.php
@@ -27,7 +27,7 @@
 		}
 
 		$stmt = mysqli_stmt_init($conn);
-		mysqli_stmt_prepare($stmt, "UPDATE comment SET comment='? WHERE comment_num = $comment_num");
+		mysqli_stmt_prepare($stmt, "UPDATE comment SET comment=? WHERE comment_num = $comment_num");
 		mysqli_stmt_bind_param($stmt, 's', $comment);
 
 		if(mysqli_stmt_execute($stmt))
