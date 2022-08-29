c271e990b4479d8feddc8f83b4fbb01f842b5508
ksh30918 <ksh30918@kakao.com>
2022-08-30 00:29:27 +0900

    edit

--- view.php
+++ view.php
@@ -14,7 +14,7 @@
 			mysqli_stmt_prepare($stmt, "SELECT * FROM board WHERE post_num=?");
 			mysqli_stmt_bind_param($stmt, 'i', $post_num);
 			mysqli_stmt_execute($stmt);
-			$result = mysqli_stmt_get_result($stmt);
+			$result = mysqli_fetch_array(mysqli_stmt_get_result($stmt));
 
 			if($result == "")
 			{
